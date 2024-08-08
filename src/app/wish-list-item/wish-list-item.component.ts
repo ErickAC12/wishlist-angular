ba390b5e3298d8eb523899321d97f9288b3b540f
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-08-07 18:12:12 -0600

    wish items are crossed when completed

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'wish-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css'
 })
@@ -11,6 +12,11 @@ export class WishListItemComponent {
   @Input() wishText!: string;
   @Input() fulfilled!: boolean;
   @Output() fulfilledChange = new EventEmitter<boolean>();
+
+  get cssClasses() {
+    //return this.fulfilled ? ['strikeout text-muted'] : [];
+    return { 'strikeout text-muted': this.fulfilled };
+  }
 
   toggleFulfilled() {
     this.fulfilled = !this.fulfilled;
